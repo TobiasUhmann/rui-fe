02f94b783facc34c92797fee2952b6eadd8ecf71
Tobias Uhmann <tobias.uhmann@gmail.com>
2021-07-06 09:46:50 +0200

    Add routes to Dashboard etc.

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,10 +10,22 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Dashboard.vue')
+  },
+  {
+    path: '/explore',
+    name: 'Explore',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Explore.vue')
+  },
+  {
+    path: '/taxonomy',
+    name: 'Taxonomy',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Taxonomy.vue')
   }
 ]
 
