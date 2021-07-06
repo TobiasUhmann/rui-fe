d1ee11bf7e04e33634582dbc4ff9e8aa69ad0bc3
Tobias Uhmann <tobias.uhmann@gmail.com>
2021-07-06 09:51:22 +0200

    Remove Home and About components

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,37 +1,30 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import Home from '../views/Home.vue'
+import {createRouter, createWebHistory, RouteRecordRaw} from 'vue-router'
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
-    path: '/dashboard',
-    name: 'Dashboard',
-    component: () => import(/* webpackChunkName: "about" */ '../views/Dashboard.vue')
-  },
-  {
-    path: '/explore',
-    name: 'Explore',
-    component: () => import(/* webpackChunkName: "about" */ '../views/Explore.vue')
-  },
-  {
-    path: '/taxonomy',
-    name: 'Taxonomy',
-    component: () => import(/* webpackChunkName: "about" */ '../views/Taxonomy.vue')
-  }
+    {
+        path: '/',
+        redirect: '/dashboard'
+    },
+    {
+        path: '/dashboard',
+        name: 'Dashboard',
+        component: () => import(/* webpackChunkName: 'dashboard' */ '../views/Dashboard.vue')
+    },
+    {
+        path: '/explore',
+        name: 'Explore',
+        component: () => import(/* webpackChunkName: 'explore' */ '../views/Explore.vue')
+    },
+    {
+        path: '/taxonomy',
+        name: 'Taxonomy',
+        component: () => import(/* webpackChunkName: 'taxonomy' */ '../views/Taxonomy.vue')
+    }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
 })
 
 export default router
