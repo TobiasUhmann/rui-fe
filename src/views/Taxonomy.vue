af6c658b4779a0421c98be324ad99f755623bd0e
Tobias Uhmann <tobias.uhmann@gmail.com>
2021-07-14 14:52:05 +0200

    Log data to console

--- src/views/Taxonomy.vue
+++ src/views/Taxonomy.vue
@@ -20,7 +20,10 @@ export default defineComponent({
   mounted() {
     fetch('https://rui-be-3-create-databas-oemcgs.herokuapp.com/db')
         // .then(response => response.json())
-        .then(data => this.taxonomy = data)
+        .then(data => {
+          console.log(data)
+          this.taxonomy = data
+        })
   }
 })
 
