df0f928ed16f9a26a69a4dbb914d2268cfa91649
Tobias Uhmann <tobias.uhmann@gmail.com>
2021-08-12 17:58:22 +0200

    Fix missing rename entity -> name

--- src/services/MatchesService.ts
+++ src/services/MatchesService.ts
@@ -3,7 +3,7 @@ import Match from '@/models/Match'
 export default {
 
     getMatches(name: string, offset = 0, limit = 3): Promise<Match[]> {
-        return fetch(`${process.env.VUE_APP_API_URL}/matches?entity=${name}&offset=${offset}&limit=${limit}`)
+        return fetch(`${process.env.VUE_APP_API_URL}/matches?name=${name}&offset=${offset}&limit=${limit}`)
             .then(response => {
                 console.debug(response)
                 return response.json()
