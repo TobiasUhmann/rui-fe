2ea0b1254b9e57787d715f681e1d04e919b12eff
Tobias Uhmann <tobias.uhmann@gmail.com>
2021-09-09 13:49:30 +0200

    Implement PaginationBar

--- src/components/pagination-bar/pagination-bar.ts
+++ src/components/pagination-bar/pagination-bar.ts
@@ -1,5 +1,33 @@
 import {defineComponent} from 'vue'
 
 export default defineComponent({
-    name: 'PaginationBar'
+    name: 'PaginationBar',
+
+    props: {
+        numberOfPages: {
+            type: Number,
+            required: true
+        }
+    },
+
+    emits: {
+        input(currentPage: number) {
+            return true
+        }
+    },
+
+    data() {
+        return {
+            currentPage: 1
+        }
+    },
+
+    watch: {
+        currentPage: {
+            immediate: true,
+            handler(currentPage: number) {
+                this.$emit('input', currentPage)
+            }
+        }
+    },
 })
