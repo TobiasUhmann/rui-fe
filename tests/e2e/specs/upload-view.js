96df64cb1d63f0a1474c00143a003663fc3eb6cc
Tobias Uhmann <tobias.uhmann@gmail.com>
2021-09-30 14:13:30 +0200

    Use visual snapshot testing for upload page tests

--- tests/e2e/specs/upload-view.js
+++ tests/e2e/specs/upload-view.js
@@ -3,7 +3,7 @@ describe('Upload Page', () => {
     it('Load page', () => {
         cy.visit('/upload')
 
-        cy.get('html').toMatchSnapshot()
+        cy.get('html').toMatchImageSnapshot()
     })
 
     it('Load page via root URL', () => {
@@ -12,27 +12,22 @@ describe('Upload Page', () => {
         // should lead to Upload Page
         cy.url().should('contain', '/upload')
 
-        cy.get('html').toMatchSnapshot()
+        cy.get('html').toMatchImageSnapshot()
     })
 
     it('Upload valid ZIP', () => {
         cy.visit('/upload')
 
-        cy.fixture('symptax_upload_v7_random.zip').then(fileContent => {
-            cy.get('input[type="file"]').attachFile({
-                fileContent: fileContent,
-                fileName: 'symptax_upload_v7_random.zip'
-            })
+        cy.get('input[type="file"]').attachFile('symptax_upload_v7_random.zip')
 
-            // click "Upload"
-            cy.get('[type="submit"]').click()
-            cy.get('.continue').click()
+        // click "Upload"
+        cy.get('[type="submit"]').click()
+        cy.get('.continue').click()
 
-            // should lead to Taxonomy Page
-            cy.url().should('contain', '/taxonomy')
+        // should lead to Taxonomy Page
+        cy.url().should('contain', '/taxonomy')
 
-            cy.get('.node-name').should('contain', 'C-1')
-            cy.get('html').toMatchSnapshot()
-        })
+        cy.get('.node-name').should('contain', 'C-1')
+        cy.get('html').toMatchImageSnapshot()
     })
 })
