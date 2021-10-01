47c4bba2f690f7e82698aee684f6cfa93371fac8
Tobias Uhmann <tobias.uhmann@gmail.com>
2021-10-01 11:35:27 +0200

    Add prediction page tests

--- tests/e2e/specs/predictions-page.js
+++ tests/e2e/specs/predictions-page.js
@@ -1,12 +1,39 @@
 describe('Predictions Page', () => {
 
-    it('Load Page', () => {
+    it('Upload ZIP', () => {
+
+        cy.visit('/upload')
+
+        cy.get('input[type="file"]').attachFile('symptax_upload_v7_random.zip')
+        cy.get('[type="submit"]').click()
+        cy.get('.continue').click()
+
+        cy.url().should('contain', '/taxonomy')
+        cy.get('html').should('contain', 'C-1')
+        cy.get('.grid').toMatchImageSnapshot()
+    })
+
+    it('Load page', () => {
+
+        // WHEN loading the "Predictions" page for a certain node via URL
+        // THEN the page should be shown with the node's predictions
+
         cy.visit('/predictions/0')
 
-        // Header and Footer should be shown
-        cy.checkHeaderAndFooter()
+        cy.contains('Porta lorem').should('exist')
 
-        // No nav link should be highlighted
-        cy.get('.router-link-active').should('not.exist')
+        cy.get('.app > main').toMatchImageSnapshot()
+    })
+
+    it('Dismiss prediction', () => {
+
+        // WHEN dismissing a prediction
+        // THEN the prediction should vanish
+
+        cy.get('button').contains('Dismiss').first().click()
+
+        cy.contains('Erat imperdiet').should('not.exist')
+
+        cy.get('.app > main').toMatchImageSnapshot()
     })
 })
