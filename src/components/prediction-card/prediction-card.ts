f942e1363857cf8e4908d7de6a008589a7bffc2e
Tobias Uhmann <tobias.uhmann@gmail.com>
2021-10-20 11:08:30 +0200

    Implement annotate()

--- src/components/prediction-card/prediction-card.ts
+++ src/components/prediction-card/prediction-card.ts
@@ -3,6 +3,8 @@ import {defineComponent, PropType} from 'vue'
 import {CandidateWithPredictions} from '@/models/prediction/candidate-with-predictions'
 import {getNodeName} from '@/models/node/node'
 import {PostNode} from '@/models/node/post-node'
+import {PostEntity} from "@/models/entity/post-entity";
+import {CandidatePrediction} from "@/models/prediction/candidate-prediction";
 
 export default defineComponent({
     name: 'PredictionCard',
@@ -46,6 +48,10 @@ export default defineComponent({
             return true
         },
 
+        createEntity(postEntity: PostEntity) {
+            return true
+        },
+
         createNode(postNode: PostNode) {
             return true
         }
@@ -59,19 +65,53 @@ export default defineComponent({
             mentionInput: '',
             userEditsMentionInput: false,
 
-            getNodeName: getNodeName
+            getNodeName: getNodeName,
+
+            selectedPrediction: undefined as undefined | {
+                type: 'synonym' | 'parent',
+                index: number
+            }
         }
     },
 
     methods: {
-        emitCreateNode() {
-            const mentionInput = this.$refs.mention as HTMLInputElement
-            const postNode = {
-                parentId: this.selectedNodeId,
-                entities: [{name: mentionInput.value}]
-            }
 
-            this.$emit('createNode', postNode)
+        /**
+         * Emit "createEntity" event if synonym prediction is selected or "createNode"
+         * event if child prediction is selected.
+         *
+         * Must only be called when a prediction is selected.
+         */
+        annotate() {
+            const mentionInput = this.$refs.mention as HTMLInputElement
+            const mention = mentionInput.value
+
+            const selectedPrediction = this.selectedPrediction!
+
+            if (selectedPrediction.type === 'synonym') {
+                const synonymPredictions = this.candidateWithPredictions.synonymPredictions
+                const selectedSynonymPrediction: CandidatePrediction = synonymPredictions[selectedPrediction.index]
+                const selectedNode = selectedSynonymPrediction.node
+
+                const postEntity: PostEntity = {
+                    nodeId: selectedNode.id,
+                    name: mention
+                }
+
+                this.$emit('createEntity', postEntity)
+
+            } else if (selectedPrediction.type === 'parent') {
+                const parentPredictions = this.candidateWithPredictions.parentPredictions
+                const selectedParentPrediction: CandidatePrediction = parentPredictions[selectedPrediction.index]
+                const selectedNode = selectedParentPrediction.node
+
+                const postNode: PostNode = {
+                    parentId: selectedNode.id,
+                    entities: [{name: mention}]
+                }
+
+                this.$emit('createNode', postNode)
+            }
         }
     }
 })
