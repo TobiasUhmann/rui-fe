edc615e502f3c908bdab857fa09952a5dab30382
Tobias Uhmann <tobias.uhmann@gmail.com>
2021-10-25 10:28:05 +0200

    Show loading indicator during upload

--- src/pages/upload-page/upload-page.spec.ts
+++ src/pages/upload-page/upload-page.spec.ts
@@ -35,3 +35,38 @@ it('Render', async () => {
     const uploadWarning = wrapper.findComponent(UploadWarning)
     expect(uploadWarning.classes()).toContain('d-none')
 })
+
+
+it('Click upload button', async () => {
+
+    //
+    // GIVEN the upload page with a selected file
+    //
+
+    const wrapper = await shallowMount(UploadPage)
+
+
+
+    //
+    // THEN  upload form should be rendered
+    // AND   the submit button should be disabled
+    //
+
+    const form = wrapper.find('form')
+    expect(form.text()).toContain('Upload ZIP')
+    expect(form.find('input[type="submit"]').attributes().disabled).toBe('')
+
+    //
+    // THEN  the loading indicator should not be shown
+    //
+
+    const loading = wrapper.findComponent(Loading)
+    expect(loading.classes()).toContain('d-none')
+
+    //
+    // THEN  the upload warning should not be shown
+    //
+
+    const uploadWarning = wrapper.findComponent(UploadWarning)
+    expect(uploadWarning.classes()).toContain('d-none')
+})
--- src/pages/upload-page/upload-page.ts
+++ src/pages/upload-page/upload-page.ts
@@ -14,7 +14,10 @@ export default defineComponent({
     data() {
         return {
             isFileSelected: false,
-            showUploadWarning: false
+            showUploadWarning: false,
+
+            showLoading: false,
+            showLoadingTimeout: -1
         }
     },
 
@@ -24,10 +27,16 @@ export default defineComponent({
             const form = this.$refs.form as HTMLFormElement
 
             const formData = new FormData(form)
-            UploadService.putUpload(formData)
-                .then(() => this.$router.push('/taxonomy'))
+            UploadService.putUpload(formData).then(() => {
+                this.$router.push('/taxonomy')
+
+                this.showLoading = false
+                window.clearTimeout(this.showLoadingTimeout)
+            })
 
             this.showUploadWarning = false
+
+            this.showLoadingTimeout = window.setTimeout(() => this.showLoading = true, 500)
 
             form.reset()
         }
